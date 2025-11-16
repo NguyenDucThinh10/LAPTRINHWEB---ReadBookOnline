1d026a54e1782e53b12cdfdca73ecec65da98f54
NguyenThanhTiep <123tiepnguyenthanh@gmail.com>
2025-11-16 23:05:26 +0700

    Cập nhật file config/database.php

--- config/database.php
+++ config/database.php
@@ -1,12 +1,11 @@
-﻿﻿<?php
+﻿<?php
 // config/database.php
 
 return [
 
     'host' => 'localhost',
-    'port' => 3306,
     'dbname' => 'onlinelibrary',
     'username' => 'root',        // Đổi tên 'username' thành 'user' cho nhất quán với code của bạn
     'password' => '',            // NHẬP MẬT KHẨU NẾU CÓ
-    'charset' => 'utf8mb4'       
+    'charset' => 'utf8mb4'       // Giữ lại charset, rất tốt
 ];
